fd599ec25db3e5f9a2eae6606af285928f009083
Zin Mar Myo Lwin <zinmarmyolwin@acedatasystems.com>
2024-10-13 15:21:04 +0630

    Add project files.

---
+++ VendingMachineSystem.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendingMachineSystem.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
